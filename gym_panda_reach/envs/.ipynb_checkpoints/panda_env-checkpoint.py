b4cf1cfb90369d041b1c1fb4c1b44197f9a404d9
mcarfagno <m.carfagno@outlook.com>
2020-07-31 11:38:16 +0100

    gripper orientation and camera position

--- gym_panda_reach/envs/.ipynb_checkpoints/panda_env-checkpoint.py
+++ gym_panda_reach/envs/.ipynb_checkpoints/panda_env-checkpoint.py
@@ -18,13 +18,13 @@ class PandaEnv(gym.Env):
     def __init__(self):
         self.step_counter = 0
         p.connect(p.GUI,options='--background_color_red=0.0 --background_color_green=0.929--background_color_blue=0.549')
-        p.resetDebugVisualizerCamera(cameraDistance=1.5, cameraYaw=0, cameraPitch=-40, cameraTargetPosition=[0.55,-0.35,0.2])
+        p.resetDebugVisualizerCamera(cameraDistance=1.5, cameraYaw=90, cameraPitch=-40, cameraTargetPosition=[0.55,-0.35,0.2])
         self.action_space = spaces.Box(np.array([-1]*4), np.array([1]*4))
         self.observation_space = spaces.Box(np.array([-1]*5), np.array([1]*5))
 
     def step(self, action):
         p.configureDebugVisualizer(p.COV_ENABLE_SINGLE_STEP_RENDERING)
-        orientation = p.getQuaternionFromEuler([0.,-math.pi,math.pi/2.])
+        orientation = p.getQuaternionFromEuler([0.,-math.pi,-math.pi])
         dv = 0.005
         dx = action[0] * dv
         dy = action[1] * dv
@@ -72,14 +72,15 @@ class PandaEnv(gym.Env):
         urdfRootPath=pybullet_data.getDataPath()
         p.setGravity(0,0,-10)
 
-        planeUid = p.loadURDF(os.path.join(urdfRootPath,"plane.urdf"), basePosition=[0,0,-0.65])
+        #planeUid = p.loadURDF(os.path.join(urdfRootPath,"plane.urdf"), basePosition=[0,0,-0.65])
 
-        rest_poses = [0,-0.215,0,-2.57,0,2.356,2.356,0.08,0.08]
+        rest_poses = [0, -0.215, 0, -2.57, 0.0, 2.356, math.pi/4, 0.0, 0.0, 0.0]
+#         rest_poses = [0, 0, 0, 0, 0.0, 0, 0, 0.0, 0.0, 0.0]
         self.pandaUid = p.loadURDF(os.path.join(urdfRootPath, "franka_panda/panda.urdf"),useFixedBase=True)
-        for i in range(7):
+        
+        for i in range(len(rest_poses)):
             p.resetJointState(self.pandaUid,i, rest_poses[i])
-        p.resetJointState(self.pandaUid, 9, 0.08)
-        p.resetJointState(self.pandaUid,10, 0.08)
+            
         tableUid = p.loadURDF(os.path.join(urdfRootPath, "table/table.urdf"),basePosition=[0.5,0,-0.65])
 
         trayUid = p.loadURDF(os.path.join(urdfRootPath, "tray/traybox.urdf"),basePosition=[0.65,0,0])
